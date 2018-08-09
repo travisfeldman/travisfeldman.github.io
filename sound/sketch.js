1ac0f8da751de2dfd9281752aa29a373848691a0
travisfeldman <42243005+travisfeldman@users.noreply.github.com>
2018-08-09 23:23:41 +0200

    Update sketch.js

--- sound/sketch.js
+++ sound/sketch.js
@@ -1,47 +1,30 @@
-// Adapted from Learning Processing by Daniel Shiffman
-// http://www.learningprocessing.com
-// Doorbell sample by Corsica_S via freesound.org,
-// Creative Commons BY 3.0
-
-// A sound file object
 var dingdong;
-
-// A doorbell object (that will trigger the sound)
 var doorbell;
 
 function setup() {
   createCanvas(800, 800);
-
-  // Load the sound file.
-  // We have included both an MP3 and an OGG version.
   soundFormats('mp3', 'ogg');
-  dingdong = loadSound('travisfeldman.github.io/FILES/doorbell.mp3');
-
-  // Create a new doorbell
+  dingdong = loadSound('https://travisfeldman.github.io/FILES/doorbell.mp3');
   doorbell = new Doorbell(width/2, height/2, 128);
 }
 
 function draw() {
   background(255);
-  // Show the doorbell
   doorbell.display(mouseX, mouseY);
 }
 
 function mousePressed() {
-  // If the user clicks on the doorbell, play the sound!
   if (doorbell.contains(mouseX, mouseY)) {
     dingdong.play();
   }
 }
 
-// A Class to describe a "doorbell" (really a button)
 var Doorbell = function(x_, y_, r_) {
   // Location and size
   var x = x_;
   var y = y_;
   var r = r_;
 
-  // Is a point inside the doorbell? (used for mouse rollover, etc.)
   this.contains = function(mx, my) {
     if (dist(mx, my, x, y) < r) {
       return true;
