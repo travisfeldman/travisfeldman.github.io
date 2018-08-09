be71ae97884219258acf654a4399c7b58e77acab
travisfeldman <42243005+travisfeldman@users.noreply.github.com>
2018-08-09 15:52:01 +0200

    Update sketch.js

--- ML2/sketch.js
+++ ML2/sketch.js
@@ -14,9 +14,9 @@ let status;
 let objects = [];
 
 function setup() {
-  createCanvas(320, 240);
+  createCanvas(640, 480);
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(640, 480);
 
   // Create a YOLO method
   yolo = ml5.YOLO(video, startDetecting);
