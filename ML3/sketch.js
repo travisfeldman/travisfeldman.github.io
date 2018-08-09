3d6e6b2f359c9a38c84539ed5e6af6bfbfd9d8ef
travisfeldman <travisfeldman@gmail.com>
2018-08-09 15:47:15 +0200

    ML

---
+++ ML3/sketch.js
@@ -0,0 +1,59 @@
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using MobileNet and p5.js
+This example uses a callback pattern to create the classifier
+=== */
+
+let classifier;
+let video;
+let currentPrediction;
+
+function setup() {
+  createCanvas(400,400);
+
+  //noCanvas();
+  // Create a camera input
+  video = createCapture(VIDEO);
+  // Initialize the Image Classifier method with MobileNet and the video as the second argument
+  classifier = ml5.imageClassifier('MobileNet', video, modelReady);  
+}
+
+function draw(){
+  if (currentPrediction === "ping-pong ball") {
+    background(0,255,0);
+  }
+  else if (currentPrediction === "coffee mug") {
+    background(255,0,0);
+  }
+  else {
+    background(0,0,255);
+  }
+}
+
+function modelReady() {
+  // Change the status of the model once its ready
+  select('#status').html('Model Loaded');
+  // Call the classifyVideo function to start classifying the video
+  classifyVideo();
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  classifier.predict(gotResult);
+}
+
+// When we get a result
+function gotResult(err, results) {
+  // The results are in an array ordered by probability.
+  select('#result').html(results[0].className);
+  select('#probability').html(nf(results[0].probability, 0, 2));
+  classifyVideo();
+  currentPrediction = results[0].className;
+  console.log(currentPrediction);
+
+}
