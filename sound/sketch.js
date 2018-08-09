6c3b3cefa8ebbc32bda1e7fa6f27e1f7e668f7e1
travisfeldman <42243005+travisfeldman@users.noreply.github.com>
2018-08-09 23:33:57 +0200

    Update sketch.js

--- sound/sketch.js
+++ sound/sketch.js
@@ -1,47 +1,17 @@
-var dingdong;
-var doorbell;
+var song;
 
 function setup() {
-  createCanvas(800, 800);
-  soundFormats('mp3', 'ogg');
-  dingdong = loadSound('https://travisfeldman.github.io/FILES/doorbell.mp3');
-  doorbell = new Doorbell(width/2, height/2, 128);
-}
-
-function draw() {
-  background(255);
-  doorbell.display(mouseX, mouseY);
+  song = loadSound('https://travisfeldman.github.io/FILES/doorbell.mp3');
+  createCanvas(720, 200);
+  background(255,0,0);
 }
 
 function mousePressed() {
-  if (doorbell.contains(mouseX, mouseY)) {
-    dingdong.play();
+  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+    song.stop();
+    background(255,0,0);
+  } else {
+    song.play();
+    background(0,255,0);
   }
 }
-
-var Doorbell = function(x_, y_, r_) {
-  // Location and size
-  var x = x_;
-  var y = y_;
-  var r = r_;
-
-  this.contains = function(mx, my) {
-    if (dist(mx, my, x, y) < r) {
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  // Show the doorbell (hardcoded colors, could be improved)
-  this.display = function(mx, my) {
-    if (this.contains(mx, my)) {
-      fill(100);
-    } else {
-      fill(175);
-    }
-    stroke(0);
-    strokeWeight(4);
-    ellipse(x, y, r, r);
-  };
-};
